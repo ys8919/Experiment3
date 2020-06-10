aa841a1e837417d3c123188975ef4dddf2a3d99d
ys8919 <945284015@qq.com>
2020-06-10 08:57:22 +0800

    版本1-3 增加获取照片功能

--- app/src/main/java/com/example/experiment3/MainActivity.java
+++ app/src/main/java/com/example/experiment3/MainActivity.java
@@ -1,19 +1,45 @@
 package com.example.experiment3;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.icu.util.Calendar;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
 
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -26,10 +52,14 @@ import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
+import com.cocosw.bottomsheet.BottomSheet;
 import com.google.android.material.textfield.TextInputLayout;
 import com.lljjcoder.Interface.OnCityItemClickListener;
 import com.lljjcoder.bean.CityBean;
@@ -38,30 +68,42 @@ import com.lljjcoder.bean.ProvinceBean;
 import com.lljjcoder.citywheel.CityConfig;
 import com.lljjcoder.style.citylist.Toast.ToastUtils;
 import com.lljjcoder.style.citypickerview.CityPickerView;
-import com.luck.picture.lib.PictureSelector;
-import com.luck.picture.lib.config.PictureConfig;
-import com.luck.picture.lib.config.PictureMimeType;
-import com.luck.picture.lib.entity.LocalMedia;
+
+
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
-
-
-import static com.luck.picture.lib.config.PictureMimeType.ofImage;
+import java.util.Objects;
 
 
 @RequiresApi(api = Build.VERSION_CODES.N)
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private ImageView img_face;
+    private static final String TAG = "MainActivity";
+    //权限
+    private static final int PERMISSION_REQUEST = 1001;
+    List<String> permissionsList = new ArrayList<>();
 
+    private ImageView img_face;
+    private Context context;
+    private Uri imageUri;
+    public static final int TAKE_PHOTO = 1;
+    public static final int CHOOSE_PHOTO = 2;
+    public static final int READ_PHOTO = 3;
+
+    private String orientation = null;
     //所选相册图片的路径(原图/压缩后/剪裁后)
     String albumPath = "";
     //用来转换相机路径用的
-    LocalMedia localMedia = new LocalMedia();
+
 
     //输入限制
+    TextView textView;
     TextInputLayout layoutTextName;
     TextInputLayout layoutTextID;
     TextInputLayout layoutTextEmail;
@@ -91,7 +133,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //禁止横屏
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
+        //权限
 
+
+        textView=findViewById(R.id.textView);
         layoutTextName=findViewById(R.id.TextName);
         layoutTextID=findViewById(R.id.TextID);
         layoutTextEmail=findViewById(R.id.TextEmail);
@@ -99,13 +144,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         layoutTextDetailAddress=findViewById(R.id.TextDetailAddress);
         layoutTextDate=findViewById(R.id.TextDate);
 
-
+        context=this;
         img_face =findViewById(R.id.img_face);
         img_face.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showPicSeleDialog(MainActivity.this);
-            }
+                showSimpleBottomSheetList();
+        }
         });
 
         //地址选择
@@ -299,78 +344,304 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-    /**
-     * picSelector的相册相机界面
-     *
-     * // 例如 LocalMedia 里面返回三种 path
-     *                     // 1.media.getPath(); 为原图 path
-     *                     // 2.media.getCutPath();为裁剪后 path，需判断 media.isCut();是否为 true
-     *                     // 3.media.getCompressPath();为压缩后 path，需判断 media.isCompressed();是否为 true
-     *                     // 如果裁剪并压缩了，以取压缩路径为准，因为是先裁剪后压缩的
-     *
-     */
-    protected void showPicSeleDialog(Activity activity) {
-        // 进入相册 以下是例子：不需要的api可以不写
-        PictureSelector.create(activity)
-                .openGallery(ofImage())// 全部.PictureMimeType.ofAll()、图片.ofImage()、视频.ofVideo()、音频.ofAudio()
-                .theme(R.style.picture_default_style)// 主题样式设置 具体参考 values/styles   用法：R.style.picture.white.style
-                .maxSelectNum(1)// 最大图片选择数量
-                .selectionMode( PictureConfig.SINGLE)// 多选 or 单选
-                .isCamera(true)// 是否显示拍照按钮
-                .isZoomAnim(true)// 图片列表点击 缩放效果 默认true
-                .imageFormat(PictureMimeType.PNG)// 拍照保存图片格式后缀,默认jpeg
-                .compress(true)// 是否压缩
-                .synOrAsy(true)//同步true或异步false 压缩 默认同步
-                .compressSavePath(getCompressPath())//压缩图片自定义保存地址
-                //.sizeMultiplier(0.5f)// glide 加载图片大小 0~1之间 如设置 .glideOverride()无效
-                .glideOverride(160, 160)// glide 加载宽高，越小图片列表越流畅，但会影响列表图片浏览的清晰度
-                .forResult(PictureConfig.CHOOSE_REQUEST);//结果回调onActivityResult code
 
+
+    private void showSimpleBottomSheetList() {
+        //1、使用Dialog、设置style
+        final Dialog dialog = new Dialog(MainActivity.this,R.style.DialogTheme);
+        //2、设置布局
+        View view = View.inflate(MainActivity.this,R.layout.dialog_custom_layout,null);
+        dialog.setContentView(view);
+
+        Window window = dialog.getWindow();
+        //设置弹出位置
+        window.setGravity(Gravity.BOTTOM);
+        //设置弹出动画
+        window.setWindowAnimations(R.style.main_menu_animStyle);
+        //设置对话框大小
+        window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.show();
+
+        dialog.findViewById(R.id.tv_take_photo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                File file = new File(getExternalCacheDir(), "output_image.jpg");
+                try {
+                    if (file.exists()) {
+                        file.delete();
+                    }
+                    file.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                if (Build.VERSION.SDK_INT >= 24) {
+                   // imageUri = FileProvider.getUriForFile(MainActivity.this, "com.example.experiment3.provider", file);
+                    //Log.d(TAG, "onClick: imageUri:"+imageUri);
+                } else {
+                    imageUri = Uri.fromFile(file);
+
+                }
+
+                if (ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, TAKE_PHOTO);
+                } else {
+                    callCamera();
+                }
+                dialog.dismiss();
+            }
+        });
+
+        dialog.findViewById(R.id.tv_take_pic).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, CHOOSE_PHOTO);
+                } else if(ContextCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, READ_PHOTO);
+                } else{
+
+                    goPhotoAlbum();
+                }
+                dialog.dismiss();
+
+            }
+        });
+
+        dialog.findViewById(R.id.tv_cancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
     }
+
+
+
+    //获取权限
+
 
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) { // 如果返回码是可以用的
-            switch (requestCode) {
-                case PictureConfig.CHOOSE_REQUEST:
-                    if(PictureSelector.obtainMultipleResult(data).get(0) != null)
-                    {
-                        // 图片选择结果回调
-                        localMedia = PictureSelector.obtainMultipleResult(data).get(0);
-                        if(localMedia.isCompressed())
-                        {
-                            albumPath = localMedia.getCompressPath();
-                            img_face.setBackgroundResource(0);
-                            //设置图片圆角角度
-                            RoundedCorners roundedCorners = new RoundedCorners(30);
-                            //通过RequestOptions扩展功能
-                            RequestOptions options = RequestOptions.bitmapTransform(roundedCorners).override(300, 300)
-                                    //圆形
-                                    .circleCrop();
-                            Glide.with(MainActivity.this)
-                                    .load(albumPath).apply(options).into(img_face);
-                        }
+        switch (requestCode) {
+            case TAKE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    //Bitmap bitmap = null;
+                    //bitmap = (Bitmap) BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                    Bitmap bitmap = (Bitmap) Objects.requireNonNull(data.getExtras()).get("data");
+                    int angle = 0;
+                    Uri uri = data.getData();
+                    //String imagePath = getImagePath(uri, null);
+                    Log.d(TAG, "onActivityResult: uri"+uri);
+                    /*if (angle != 0) {
+                        Log.d(TAG, "onActivityResult: angle:"+angle);
+                        // 下面的方法主要作用是把图片转一个角度，也可以放大缩小等
+                        Matrix m = new Matrix();
+                        int width = bitmap.getWidth();
+                        int height = bitmap.getHeight();
+                        m.setRotate(angle); // 旋转angle度
+                        bitmap = Bitmap.createBitmap(bitmap, 0, 0, width, height, m, true);// 从新生成图片
+
                     }
 
-                    break;
+                     */
+                    bitmap= getCircleBitmap(bitmap);
+                    img_face.setImageBitmap(bitmap);
+                    textView.setText("");
+                }
+                break;
+            case CHOOSE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    handleImageBeforeKitKat(data);
+                    //判断手机系统版本号
+                  /*  if (Build.VERSION.SDK_INT >= 19) {
+                        //4.4及以上系统使用
+                        handleImageOnKitKat(data);
+                    } else {
+
+                    }
+
+                   */
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    //激活相册操作
+    private void goPhotoAlbum() {
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent, CHOOSE_PHOTO);
+    }
+
+    //打开相机
+    private void callCamera() {
+        //启动相机权限
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, TAKE_PHOTO);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            //打开相册
+            case CHOOSE_PHOTO:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    goPhotoAlbum();
+                } else {
+
+                    Toast.makeText(context, "您没有打开相册的权限", Toast.LENGTH_SHORT).show();
+
+                }
+                break;
+            //相机的权限
+            case TAKE_PHOTO:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    callCamera();
+                } else {
+
+                    Toast.makeText(context, "您没有打开相机的权限", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case READ_PHOTO:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //goPhotoAlbum();
+                } else {
+                    Toast.makeText(context, "您没有读取照片的权限", Toast.LENGTH_SHORT).show();
+
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void handleImageBeforeKitKat(Intent data) {
+        Uri uri = data.getData();
+        // String imagePath = getImagePath(uri, null);
+        dispalyImage(uri);
+    }
+    /*
+     @RequiresApi(api = Build.VERSION_CODES.N)
+     private void handleImageOnKitKat(Intent data) {
+         String imagePath = null;
+         Uri uri = data.getData();
+         Log.d(TAG, "handleImageOnKitKat: uri:"+uri);
+         if (DocumentsContract.isDocumentUri(this, uri)) {
+             String docId = DocumentsContract.getDocumentId(uri);
+             if ("com.android.providers.media.documents".equals(uri.getAuthority())) {
+                 String id = docId.split(":")[1];
+                 String selection = MediaStore.Images.Media._ID + "=" + id;
+                 imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection);
+             } else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())) {
+                 Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(docId));
+                 imagePath = getImagePath(contentUri, null);
+             }
+         } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+             //如果是content类型的Uri，则使用普通方式处理
+             imagePath = getImagePath(uri, null);
+         } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+             imagePath = uri.getPath();
+         }
+
+         dispalyImage(uri);
+     }
+
+
+
+     private String getImagePath(Uri uri, String selection) {
+         String path = null;
+         //通过uri和selection来获取真实的图片路径
+         Cursor cursor = getContentResolver().query(uri, null, selection, null, null);
+         if (cursor != null) {
+             if (cursor.moveToFirst()) {
+                 path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+             }
+             orientation = cursor.getString(cursor.getColumnIndex("orientation"));// 获取旋转的角度
+             cursor.close();
+         }
+         return path;
+     }
+
+
+    */
+    private void dispalyImage(Uri imagePath) {
+        if (imagePath != null) {
+            //根据path读取资源路径
+            Bitmap bitmap = null;
+            try {
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imagePath));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
             }
+
+            /*int angle = 0;
+            if (orientation != null && !"".equals(orientation)) {
+                angle = Integer.parseInt(orientation);
+            }
+            if (angle != 0) {
+                // 下面的方法主要作用是把图片转一个角度，也可以放大缩小等
+                Matrix m = new Matrix();
+                int width = bitmap.getWidth();
+                int height = bitmap.getHeight();
+                m.setRotate(angle); // 旋转angle度
+                bitmap = Bitmap.createBitmap(bitmap, 0, 0, width, height, m, true);// 从新生成图片
+            }
+
+             */
+            bitmap= getCircleBitmap(bitmap);
+            img_face.setImageBitmap(bitmap);
+            textView.setText("");
+        } else {
+            Toast.makeText(context, "获取照片失败", Toast.LENGTH_SHORT).show();
         }
     }
 
-    // 压缩后图片文件存储位置
-    private String getCompressPath() {
-        String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/PictureSelector/image/";
-        File file = new File(path);
-        if (file.mkdirs()) {
-            return path;
-        }
-        return path;
+//裁剪图片
+    public static Bitmap cropBitmap(Bitmap bitmap) {//从中间截取一个正方形
+        int w = bitmap.getWidth(); // 得到图片的宽，高
+        int h = bitmap.getHeight();
+        int cropWidth = Math.min(w, h);// 裁切后所取的正方形区域边长
+
+        return Bitmap.createBitmap(bitmap, (bitmap.getWidth() - cropWidth) / 2,
+                (bitmap.getHeight() - cropWidth) / 2, cropWidth, cropWidth);
     }
 
-
-
+    public static Bitmap getCircleBitmap(Bitmap bitmap) {//把图片裁剪成圆形
+        if (bitmap == null) {
+            return null;
+        }
+        bitmap = cropBitmap(bitmap);//裁剪成正方形
+        try {
+            Bitmap circleBitmap = Bitmap.createBitmap(bitmap.getWidth(),
+                    bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(circleBitmap);
+            final Paint paint = new Paint();
+            final Rect rect = new Rect(0, 0, bitmap.getWidth(),
+                    bitmap.getHeight());
+            final
+            RectF rectF = new RectF(new Rect(0, 0, bitmap.getWidth(),
+                    bitmap.getHeight()));
+            float roundPx = 0.0f;
+            roundPx = bitmap.getWidth();
+            paint.setAntiAlias(true);
+            canvas.drawARGB(0, 0, 0, 0);
+            paint.setColor(Color.WHITE);
+            canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+            paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+            final Rect src = new Rect(0, 0, bitmap.getWidth(),
+                    bitmap.getHeight());
+            canvas.drawBitmap(bitmap, src, rect, paint);
+            return circleBitmap;
+        } catch (Exception e) {
+            return bitmap;
+        }
+    }
 
     @Override
     public void onClick(View view) {
